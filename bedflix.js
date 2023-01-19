1ca9d699578a1fc75d19ce312af98a4aff93d44e
hamzajabre <119331309+hamzajabre@users.noreply.github.com>
2023-01-19 16:22:05 +0100

    Add files via upload

---
+++ bedflix.js
@@ -0,0 +1,60 @@
+function getMovie(movieTitle) {
+    return fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=3861f60e`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        return data;
+      });
+  }
+  
+
+  class Movie {
+    constructor(data){
+      Object.assign(this, data);
+      }
+      
+      renderMovie() {
+        return ` 
+        <div class="movie-container">
+        <div class="image-container">
+               <img src='${this.Poster}'/>
+        </div>
+        <div class="movie-content-container">
+          <div class="title">
+            <h4>${this.Title}</h4>
+            <p>${this.imdbRating}</p>
+          </div>
+          <div class="movie-details">
+            <p>${this.Runtime}</p>
+            <p>${this.Genre}</p>
+            <button class="add-watchlist-btn">Watchlist</button>
+          </div>
+          <div class="movie-desc">
+            <p>${this.Plot}</p>
+          </div>
+        </div>
+        </div>
+      `;
+      }
+    }
+    
+
+    const container = document.querySelector(".container");
+    const searchPage = document.querySelector(".search-page-container");
+    const searchBtn = document.querySelector(".search-btn");
+    const searchInput = document.querySelector(".input-search-bar");
+    
+    
+    /* If I create the instance inside of here, then anything that I need to do further, has to be nested inside. This doesn't feel right at all*/
+    searchBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      getMovie(searchInput.value).then((res) => {
+        
+        container.innerHTML += new Movie(res).renderMovie();
+        let btn = document.querySelector(".add-watchlist-btn");
+        btn.addEventListener("click", () => {
+          console.log("button clicked");
+        });
+      });
+      searchInput.value = "";
+    });
